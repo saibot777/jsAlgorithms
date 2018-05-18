566d9e7750327780dde74464a205a6b7f9e1c4f1
saibot777 <stefantrajkovic777@gmail.com>
2018-05-18 16:25:20 +0200

    forgot return

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -10,11 +10,11 @@
 function capitalize(str) {
     const words = [];
 
-    for (let word of str.split('')) {
+    for (let word of str.split(' ')) {
        words.push(word[0].toUpperCase() + word.slice(1)); 
     }
 
-    words.join(' ');
+    return words.join(' ');
 
 
 }
