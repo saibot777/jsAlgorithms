eca05ff734ec7417059a052201d4953fe535f9f0
saibot777 <stefantrajkovic777@gmail.com>
2018-11-23 02:18:45 +0100

    fibbonnacci iterative way solved

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// ITERATIVE SOLUTION
+
+function fib(n) {
+    const result = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        const a = result[i - 1];
+        const b = result[i - 2];
+
+        result.push(a + b);
+    }
+
+    return result[n];
+
+}
+
+// RECURSIVE SOLUTION
+
+// function fib(n) {}
+
+
 
 module.exports = fib;
