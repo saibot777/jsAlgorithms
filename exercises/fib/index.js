7bfdfdef072e35e2f41f1eee4b347de20c9b53bc
saibot777 <stefantrajkovic777@gmail.com>
2018-11-23 02:49:04 +0100

    recursive way with memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -26,6 +26,31 @@
 
 // RECURSIVE SOLUTION
 
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// RECURSIVE SOLUTION UPGRADED PERFOMANCE WITH MEMOIZATION
+
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args]
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result
+    }
+}
+
 function fib(n) {
     if (n < 2) {
         return n;
@@ -33,6 +58,8 @@ function fib(n) {
 
     return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoize(fib);
 
 
 
