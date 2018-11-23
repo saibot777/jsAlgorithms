3736ebc4086e779828b72987da03a021854e4081
saibot777 <stefantrajkovic777@gmail.com>
2018-11-23 02:28:26 +0100

    recursive way impl

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -10,23 +10,29 @@
 
 // ITERATIVE SOLUTION
 
-function fib(n) {
-    const result = [0, 1];
+// function fib(n) {
+//     const result = [0, 1];
 
-    for (let i = 2; i <= n; i++) {
-        const a = result[i - 1];
-        const b = result[i - 2];
+//     for (let i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
 
-        result.push(a + b);
-    }
+//         result.push(a + b);
+//     }
 
-    return result[n];
+//     return result[n];
 
-}
+// }
 
 // RECURSIVE SOLUTION
 
-// function fib(n) {}
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
 
 
 
