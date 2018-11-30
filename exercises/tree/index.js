ea87158275c544714d57f2ff21a5e38790a6f25b
saibot777 <stefantrajkovic777@gmail.com>
2018-11-30 01:13:54 +0100

    node remove method impl

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -19,8 +19,18 @@ class Node {
     add(data) {
         this.children.push(new Node(data));
     }
+
+    remove(data) {
+        this.children = this.children.filter(node => {
+            return node.data !== data;
+        })
+    }
 }
 
-class Tree {}
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+}
 
 module.exports = { Tree, Node };
