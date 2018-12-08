d55985b8b96aad3520a54eda60c018a00c6738f7
saibot777 <stefantrajkovic777@gmail.com>
2018-12-08 20:47:07 +0100

    frequencyCounter refactored

--- 2019/practices/frequencyCounter.js
+++ 2019/practices/frequencyCounter.js
@@ -3,12 +3,22 @@ function same(arr1, arr2) {
         return false;
     }
 
-    for (let i = 0; i < arr1.length; i++) {
-        let correctIndex = arr2.indexOf(arr1[i] ** 2)
-        if (correctIndex === -1) {
-            return false;
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for(let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+
+    for (let key of frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false
         }
-        arr2.splice(correctIndex, 1)
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false
+        }
     }
     return true;
 }
