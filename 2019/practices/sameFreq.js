933b50100b43c151914cf1611c3bbbc72adaa6af
saibot777 <stefantrajkovic777@gmail.com>
2018-12-09 23:40:26 +0100

    sameFreq solved

---
+++ 2019/practices/sameFreq.js
@@ -0,0 +1,15 @@
+function sameFrequency(n1, n2){
+    const arrN1 = [...String(n1)];
+    const arrN2 = [...String(n2)];
+    
+    if (arrN1.length !== arrN2.length) return false
+ 
+    const hashTable = arrN1.reduce((acc, next) => {
+        acc[next] = ++acc[next] || 1;
+        return acc;
+    }, {});
+  
+  return arrN2.every(n => 
+    !hashTable[n] ? false : (--hashTable[n], true)
+  );
+}
