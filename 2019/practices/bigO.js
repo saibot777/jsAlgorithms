114f3df1d1955724ae3eb4ca63f51bdde9ba9bf4
saibot777 <stefantrajkovic777@gmail.com>
2019-01-07 08:03:22 +0100

    refactoring...

--- 2019/practices/bigO.js
+++ 2019/practices/bigO.js
@@ -1,16 +1,28 @@
-const data = new Array(100000).fill('nemo');
+// const data = new Array(100000).fill('nemo');
 
-function find(array) {
-    let t0 = performance.now();
+// function find(array) {
+//     let t0 = performance.now();
 
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === 'nemo') {
+//             console.log('Found NEMO!');
+//         }
+//     }
+
+//     let t1 = performance.now();
+//     console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
+// }
+
+// find(data); // O(n) --> Linear Time
+
+// O(n^2) -- Log all pairs of array
+const boxes = [1,2,3,4];
+function solution(array) {
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === 'nemo') {
-            console.log('Found NEMO!');
+        for(let j = 0; j < array.length; j++) {
+            console.log(array[i], array[j])
         }
     }
-
-    let t1 = performance.now();
-    console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 
-find(data); // O(n) --> Linear Time
+solution(boxes);
