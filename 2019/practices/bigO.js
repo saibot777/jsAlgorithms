32f807b1ed0521de01622872d13edb89584da912
saibot777 <stefantrajkovic777@gmail.com>
2019-01-07 07:43:39 +0100

    bigO

--- 2019/practices/bigO.js
+++ 2019/practices/bigO.js
@@ -1,12 +1,16 @@
-// First function version
-const data = ['nemo'];
+const data = new Array(100000).fill('nemo');
 
 function find(array) {
+    let t0 = performance.now();
+
     for (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo') {
             console.log('Found NEMO!');
         }
     }
+
+    let t1 = performance.now();
+    console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 
-find(data);
+find(data); // O(n) --> Linear Time
