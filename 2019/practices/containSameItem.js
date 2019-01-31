2427eccce08e48f8397ad3b3cf8e5cf46e902f18
saibot777 <stefantrajkovic777@gmail.com>
2019-02-01 00:01:42 +0100

    common item second solution impl

--- 2019/practices/containSameItem.js
+++ 2019/practices/containSameItem.js
@@ -1,5 +1,5 @@
 const array1 = ['a', 'b', 'c', 'x'];
-const array2 = ['z', 'y', 'x'];
+const array2 = ['z', 'y', 'f'];
 
 // First way
 function containSameItem(arr1, arr2) {
@@ -17,7 +17,20 @@ console.log(containSameItem(array1, array2))
 
 // Second way
 function containSameItem2(arr1, arr2) {
-    
+    let map = {};
+    for (let i=0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = array1[i];
+            map[item] = true;
+        }
+    }
+
+    for (let j=0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+    return false
 }
 
 console.log(containSameItem2(array1, array2))
