d29e952e3b97b76f0be9617a85a87e406fd06bb2
saibot777 <stefantrajkovic777@gmail.com>
2019-02-03 20:17:05 +0100

    initiating linkedList

---
+++ 2019/practices/linkedList.js
@@ -0,0 +1,31 @@
+// let myLinkedList = {
+//     head: {
+//         value: 10,
+//         next: {
+//             value: 5,
+//             next: {
+//                 value: 16,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1
+    }
+
+    append(value) {
+        // ToDo
+    }
+}
+
+const myLinkedList = new LinkedList(10)
+
+console.log(myLinkedList)
