7e225bfa68d815c7ebfc00753d7ef0e68d826d3c
saibot777 <stefantrajkovic777@gmail.com>
2019-02-03 11:19:56 +0100

    firstRecurringChar solution impl

---
+++ 2019/practices/firstRecurringChar.js
@@ -0,0 +1,12 @@
+function firstRecurringCharacter(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] === input[j]) {
+                return input[i]
+            }
+        }
+    }
+    return undefined;
+}
+
+console.log(firstRecurringCharacter([1,5,1,2,3,5,1,2,4]))
