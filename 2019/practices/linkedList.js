d8b7859224de647582517a190a0ba653c8bfd5c0
saibot777 <stefantrajkovic777@gmail.com>
2019-02-03 21:18:18 +0100

    linkedList printList impl

--- 2019/practices/linkedList.js
+++ 2019/practices/linkedList.js
@@ -32,11 +32,28 @@ class LinkedList {
 
         return this;
     }
+
+    printList() {
+        const array = []
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+
+        return array;
+    }
+
+    insert(index, value) {
+
+    }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.printList();
+// myLinkedList.insert(2, 99);
 
-console.log(myLinkedList)
+console.log(myLinkedList.printList())
