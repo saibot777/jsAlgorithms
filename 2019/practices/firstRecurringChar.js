49d71c67fce3318d3c58e7c7ba9c86723a1d7573
saibot777 <stefantrajkovic777@gmail.com>
2019-02-03 11:26:13 +0100

    firstRecurringChar second solution impl

--- 2019/practices/firstRecurringChar.js
+++ 2019/practices/firstRecurringChar.js
@@ -1,3 +1,4 @@
+// First solution - O(n^2)
 function firstRecurringCharacter(input) {
     for (let i = 0; i < input.length; i++) {
         for (let j = i + 1; j < input.length; j++) {
@@ -10,3 +11,18 @@ function firstRecurringCharacter(input) {
 }
 
 console.log(firstRecurringCharacter([1,5,1,2,3,5,1,2,4]))
+
+// Second solution - O(n)
+function firstRecurringCharacter2(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        if (map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i;
+        }
+    }
+    return undefined;
+}
+
+console.log(firstRecurringCharacter2([1,5,1,2,3,5,1,2,4]))
