77d50fd6b90977c2c4f59df9ac8bbd53ed875742
saibot777 <stefantrajkovic777@gmail.com>
2019-02-07 10:55:56 +0100

    pop method implemented

--- 2019/practices/stackList.js
+++ 2019/practices/stackList.js
@@ -30,8 +30,30 @@ class Stack {
         return this;
     }
 
+    pop() {
+        if (!this.pop) {
+            return null;
+        }
+
+        if (this.pop === this.bottom) {
+            this.bottom = null;
+        }
+
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+
 }
 
 const myStack = new Stack();
 
 console.log(myStack.push('google'));
+console.log(myStack.push('udemy'));
+console.log(myStack.push('course'));
+
+console.log(myStack.peek());
+
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
