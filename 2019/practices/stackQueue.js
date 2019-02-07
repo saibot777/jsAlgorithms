085741c06907a9a1a86b6d358063aca0666c62ee
saibot777 <stefantrajkovic777@gmail.com>
2019-02-07 11:12:44 +0100

    initiating stack queue

---
+++ 2019/practices/stackQueue.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    peek() {
+        
+    }
+
+    enqueue(value) {
+        
+    }
+
+    dequeue() {
+        
+    }
+}
+
+const myStack = new Queue();
+
