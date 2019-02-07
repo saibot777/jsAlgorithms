957da556e51e73803c13d690db059c6de1f86c9e
saibot777 <stefantrajkovic777@gmail.com>
2019-02-07 10:34:38 +0100

    peek, push methods implemented

--- 2019/practices/stackList.js
+++ 2019/practices/stackList.js
@@ -12,4 +12,26 @@ class Stack {
         this.length = 0
     }
 
+    peek() {
+        return this.top;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+
 }
+
+const myStack = new Stack();
+
+console.log(myStack.push('google'));
