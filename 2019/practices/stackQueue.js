c975721fc3fa9c1f362a2052f051904f776704a5
saibot777 <stefantrajkovic777@gmail.com>
2019-02-07 11:27:28 +0100

    stack queue methods impl

--- 2019/practices/stackQueue.js
+++ 2019/practices/stackQueue.js
@@ -13,11 +13,20 @@ class Queue {
     }
 
     peek() {
-        
+        return this.first;
     }
 
     enqueue(value) {
-        
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
     }
 
     dequeue() {
@@ -26,4 +35,7 @@ class Queue {
 }
 
 const myStack = new Queue();
+
+console.log(myStack.enqueue('Stefan'));
+console.log(myStack.enqueue('Sladjana'));
 
