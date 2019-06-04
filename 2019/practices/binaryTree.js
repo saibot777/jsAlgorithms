9eba86186b0c50bb876ebbec714df6517f3619d1
saibot777 <stefantrajkovic777@gmail.com>
2019-06-04 17:58:33 +0200

    binaryTree - lookup impl

--- 2019/practices/binaryTree.js
+++ 2019/practices/binaryTree.js
@@ -36,7 +36,22 @@ class BinarySearchTree {
     }
 
     lookup(value) {
+        if (!this.root) {
+            return false;
+        }
 
+        let currentNode = this.root;
+        while(currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode.currentNode.right;
+            } else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+
+        return false;
     }
 }
 
