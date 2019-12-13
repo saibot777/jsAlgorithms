1953ab7286f004471311f801c7e77655a340f7f4
saibot777 <stefantrajkovic777@gmail.com>
2019-12-13 14:15:53 +0100

    proved the memo with fibs

--- 2019/practices/cache/memoization.js
+++ 2019/practices/cache/memoization.js
@@ -3,18 +3,22 @@ function addTo80(n) {
     return n + 80;
 }
 
-let cache = {
-    5: 85
-};
+
 
 function memoizedAddTo80(n) {
-    if (n in cache) {
-        return cache[n];
-    } else {
-        console.log('long time')
-        cache[n] = 5 + 80;
-        return cache[n];
-    }
+    let cache = {};
+    return function() {
+        if (n in cache) {
+            return cache[n];
+        } else {
+            console.log('long time')
+            cache[n] = 5 + 80;
+            return cache[n];
+        }
+    };
 }
 
-console.log(memoizedAddTo80(5));
+const memoized = memoizedAddTo80();
+
+console.log('1', memoized(5));
+console.log('2', memoized(5));
