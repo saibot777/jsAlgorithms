85390c040bc10174db36eb1d10f6f4da5d8ec04c
saibot777 <stefantrajkovic777@gmail.com>
2020-06-24 13:46:09 +0200

    prime nums solved

--- 2020/app.js
+++ 2020/app.js
@@ -17,12 +17,27 @@
 // console.log(sumNumbers([1, 3, 10, 50]));
 
 // ALGOS
+
+// Fibonnaci
 function fib(n) {
   const numbers = [1, 1];
   for (let i = 2; i < n + 1; i++) {
-    numbers.push(numbers[i - 2] + numbers[i - 1]);
+    numbers.push(numbers[i - 2] + numbers[i - 1]); // O(n)
   }
   return numbers[n];
 }
 console.log(fib(4));
 console.log(fib(5));
+
+// Prime Numbers
+function isPrime(number) {
+  for (let i = 2; i < Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(5));
+console.log(isPrime(9));
