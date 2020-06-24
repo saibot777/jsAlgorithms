50ebb8ee6100830f8d20acd0d34c9361cd3a7efd
saibot777 <stefantrajkovic777@gmail.com>
2020-06-24 13:24:42 +0200

    fibonnaci solved

--- 2020/app.js
+++ 2020/app.js
@@ -1,3 +1,5 @@
+// BIG O
+
 // function sumNumbers(numbers) {
 //   return numbers[0] + numbers[1] + numbers[2]; // Constant Time Complexity O(1) - fastest
 // }
@@ -13,3 +15,14 @@
 // }
 
 // console.log(sumNumbers([1, 3, 10, 50]));
+
+// ALGOS
+function fib(n) {
+  const numbers = [1, 1];
+  for (let i = 2; i < n + 1; i++) {
+    numbers.push(numbers[i - 2] + numbers[i - 1]);
+  }
+  return numbers[n];
+}
+console.log(fib(4));
+console.log(fib(5));
