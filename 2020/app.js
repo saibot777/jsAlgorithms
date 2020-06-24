bc27d7e0e4d3c94a5246593a887dcecd6cb4fb52
saibot777 <stefantrajkovic777@gmail.com>
2020-06-24 11:40:32 +0200

    Big O - linear O(n) solution

--- 2020/app.js
+++ 2020/app.js
@@ -1,5 +1,15 @@
 // function sumNumbers(numbers) {
-//   return numbers[0] + numbers[1] + numbers[2]; // O(1) - fastest
+//   return numbers[0] + numbers[1] + numbers[2]; // Constant Time Complexity O(1) - fastest
 // }
 
 // console.log(sumNumbers([1, 3, 10]));
+
+// function sumNumbers(numbers) {
+//   let result = 0;
+//   for (const number of numbers) {
+//     result += number; // Linear Time Complexity O(n)
+//   }
+//   return result;
+// }
+
+// console.log(sumNumbers([1, 3, 10, 50]));
