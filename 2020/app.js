a4c3c14181ff4ddfe9b08e25f1d21cf1c1afb44a
saibot777 <stefantrajkovic777@gmail.com>
2020-08-12 15:06:52 +0200

    knapsack with memoization impl

--- 2020/app.js
+++ 2020/app.js
@@ -130,25 +130,32 @@
 //   return permutations;
 // }
 
-function knapsack(items, cap, itemIndex) {
+function knapsackFn(items, cap, itemIndex, mem) {
+  console.log("RUNNING");
+  if (mem[cap][itemIndex]) {
+    return memo[cap][itemIndex];
+  }
   if (cap === 0 || itemIndex < 0) {
     return { items: [], value: 0, weight: 0 };
   }
 
   if (cap < items[itemIndex].weight) {
-    return knapsack(items, cap, itemIndex - 1);
+    return knapsackFn(items, cap, itemIndex - 1, mem);
   }
 
-  const sackWithItem = knapsack(
+  const sackWithItem = knapsackFn(
     items,
     cap - items[itemIndex].weight,
-    itemIndex - 1
+    itemIndex - 1,
+    mem
   );
 
-  const sackWithoutItem = knapsack(items, cap, itemIndex - 1);
+  const sackWithoutItem = knapsackFn(items, cap, itemIndex - 1, mem);
 
   const valueWithItem = sackWithItem.value + items[itemIndex].value;
   const valueWithoutItem = sackWithoutItem.value;
+
+  let resultSack;
 
   if (valueWithItem > valueWithoutItem) {
     const updatedSack = {
@@ -156,10 +163,21 @@ function knapsack(items, cap, itemIndex) {
       value: sackWithItem.value + items[itemIndex].value,
       weight: sackWithItem.weight + items[itemIndex].weight,
     };
-    return updatedSack;
+    resultSack = updatedSack;
   } else {
-    return sackWithoutItem;
+    resultSack = sackWithoutItem;
   }
+
+  mem[cap][itemIndex] = resultSack;
+
+  return resultSack;
+}
+
+function knapsack(items, cap, index) {
+  const mem = Array.from(Array(cap + 1), () =>
+    Array(items.length).fill(undefined)
+  );
+  return knapsackFn(items, cap, index, mem);
 }
 
 const maxCap = 8;
