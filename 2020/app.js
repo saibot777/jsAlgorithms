0df30bf1d2e4e8c30649d9c307e56bee6b68bf20
saibot777 <stefantrajkovic777@gmail.com>
2020-08-12 11:09:09 +0200

    permutations impl

--- 2020/app.js
+++ 2020/app.js
@@ -103,3 +103,29 @@ function factorial(number) {
 console.log(factorial(3));
 console.log(factorial(4));
 console.log(factorial(5));
+
+function getPermutations(options) {
+  const permutations = [];
+
+  if (options.length === 1) {
+    return [options];
+  }
+
+  const partialPermutations = getPermutations(options.slice(1));
+
+  const firstOption = options[0];
+
+  for (let i = 0; i < partialPermutations.length; i++) {
+    const partialPermutation = partialPermutations[i];
+
+    for (let j = 0; j <= partialPermutation.length; j++) {
+      const permutationInFront = partialPermutation.slice(0, j);
+      const permutationAfter = partialPermutation.slice(j);
+      permutations.push(
+        permutationInFront.concat([firstOption], permutationAfter)
+      );
+    }
+  }
+
+  return permutations;
+}
