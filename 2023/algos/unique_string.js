17b679d01e66369f201a7b157711adeb5e2bcb1f
saibot777 <stefantrajkovic777@gmail.com>
2022-10-27 21:40:13 +0200

    Add unique string algo faster

--- 2023/algos/unique_string.js
+++ 2023/algos/unique_string.js
@@ -5,11 +5,35 @@ console.log(
   isUnique("abcdaef") // -> false
 );
 
+// time complexity O(n^2))
 function isUnique(str) {
   for (let i = 0; i < str.length; i++) {
     if (str.lastIndexOf(str[i]) !== i) {
       return false;
     }
+  }
+
+  return true;
+}
+
+console.log(
+  isUnique2("abcdef"), // -> true
+  isUnique2("89%df#$a^x"), // -> true
+  isUnique2("abcAdef"), // -> true
+  isUnique2("abcdaef") // -> false
+);
+
+// even better time complexity O(n)
+function isUnique2(str) {
+  const chars = {};
+  for (let i = 0; i < str.length; i++) {
+    const thisChar = str[i];
+
+    if (chars[thisChar] === true) {
+      return false;
+    }
+
+    chars[thisChar] = true;
   }
 
   return true;
